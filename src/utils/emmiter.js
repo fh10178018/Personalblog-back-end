ba303d8cf463a6813e4ce8569ea5a12e0367c39d
fh10178018 <1206758827@qq.com>
2020-09-22 21:35:52 +0800

    修改昨天的忽略的错误

--- src/utils/emmiter.js
+++ src/utils/emmiter.js
@@ -1,12 +1,19 @@
+// 非常经典的事件中心的实现，把所有的事件用vm._events存储起来，
+// 当执行vm.$on(event,fn)的时候，按事件的名称event把回调函数fn存储起来vm._events[event].push(fn)。
+// 当执行vm.$emit(event)的时候，根据事件名event找到所有的回调函数let cbs = vm._events[event]，然后遍历执行所有的回调函数。
+// 当执行vm.$off(event,fn)的时候会移除指定事件名event和指定的fn 。
+// 当执行vm.$once(event,fn)的时候，内部就是执行vm.$on，并且当回调函数执行一次后再通过vm.$off移除事件的回调，这样就确保了回调函数只执行一次。
+
 // getCurrentInstance获得当前组件的实例
 import { getCurrentInstance } from 'vue'
 import { capitalize } from '../config/capitalize'
 
-// Symbol 本质上是一种唯一标识符
-// 可用作对象的唯一属性名，这样其他人就不会改写或覆盖你设置的属性值。
+// 建立一个ELEMENT_EVENTS名称的Symbol数据类型
+// 在这里他被名为props中属性名称，防止重复
+// 该数据类型，是全局唯一的值
 const EVENT_NAME_KEY = Symbol('ELEMENT_EVENTS')
 
-export function useEmitter(instance = getCurrentInstance()) {
+export function useEmitter (instance = getCurrentInstance()) {
   return {
     on: on(instance),
     dispatch: dispatch(instance)
@@ -30,12 +37,12 @@ const on = instance => {
     // 判断该节点的props对象中，是否命名了该对象
     // 没有新建该对象数组
     // new set为ES6属性，该属性返回一个数组对象，且不重复
-    if (!props) {
+    if (!instance.vnode.props[EVENT_NAME_KEY]) {
       instance.vnode.props[EVENT_NAME_KEY] = new Set()
     }
     // 并给在数组中添加和记录新的监听事件名称
     instance.vnode.props[EVENT_NAME_KEY].add(eventName)
-    console.log(instance.vnode.props)
+    console.log(instance.vnode.props, instance.vnode.props[EVENT_NAME_KEY])
     if (!instance.vnode.props[eventName]) {
       instance.vnode.props[eventName] = (...params) => {
         const callbacks = instance.vnode.props[eventName].__events
@@ -45,7 +52,7 @@ const on = instance => {
           })
         }
       }
-      instance.vnode.props[eventName].__events = []
+      instance.vnode.props[eventName].__events = [] // 新建__events,用于存放回调函数
     }
     instance.vnode.props[eventName].__events.push(callback)
   }
@@ -58,6 +65,7 @@ const on = instance => {
 const dispatch = instance => {
   return (componentName, eventName, ...params) => {
     let parent = instance.parent // 获取父组件的实例（instance）
+    eventName = capitalize(eventName)
     while (parent && parent.type.name !== componentName) { // 父亲找不到，就只能选择爷爷了
       parent = parent.parent
     }
