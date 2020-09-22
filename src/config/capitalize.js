f6636974618cc574088d631b37c38f2eb346b4ce
fh10178018 <1206758827@qq.com>
2020-09-22 21:36:45 +0800

    优化函数__首字母大写,遇到‘-’,字符串驼峰化处理

--- src/config/capitalize.js
+++ src/config/capitalize.js
@@ -1,5 +1,10 @@
-// 首字母大写
+// 首字母大写,遇到‘-’,字符串驼峰化处理
 export const capitalize = function (str) {
-  if (!isString(str)) return str
-  return str.charAt(0).toUpperCase() + str.slice(1)
+  if (typeof str !== 'string') return str
+  str = str.split(/[-]/g)
+  let newStr = ''
+  for (const line of str) {
+    newStr += line.charAt(0).toUpperCase() + line.slice(1)
+  }
+  return newStr
 }
