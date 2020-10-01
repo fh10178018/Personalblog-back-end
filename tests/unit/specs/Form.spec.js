42b1ac8fb17412b13a2fa087a488dd52ec801827
fh10178018 <1206758827@qq.com>
2020-10-01 21:35:07 +0800

    初始化测试，待添加

---
+++ tests/unit/specs/Form.spec.js
@@ -0,0 +1,13 @@
+import { createVue, destroyVM, waitImmediate } from '../util'
+import { mount } from '@vue/test-utils';
+import Form from '../../../src/components/common/Form/Form'
+import FormItem from '../../../src/components/common/FormItem/FormItem'
+
+describe('Form & FormItem',() => {
+  let vm
+  //测试用例后销毁
+  afterEach(() => {
+    destroyVM(vm)
+  })
+  it()
+})
