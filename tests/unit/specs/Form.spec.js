e171c3c13fac0105032ed0e0703dbce0994677c6
fh10178018 <1206758827@qq.com>
2020-10-02 19:48:26 +0800

    添加了创建测试用例，已通过，但存在警告

--- tests/unit/specs/Form.spec.js
+++ tests/unit/specs/Form.spec.js
@@ -2,6 +2,7 @@ import { createVue, destroyVM, waitImmediate } from '../util'
 import { mount } from '@vue/test-utils';
 import Form from '../../../src/components/common/Form/Form'
 import FormItem from '../../../src/components/common/FormItem/FormItem'
+import InputString from '../../../src/components/common/InputString'
 
 describe('Form & FormItem',() => {
   let vm
@@ -9,5 +10,22 @@ describe('Form & FormItem',() => {
   afterEach(() => {
     destroyVM(vm)
   })
-  it()
+  it('create', () => {
+    vm = createVue({
+      template: `
+      <Form :model="form" size="small">
+        <FormItem>
+          <InputString v-model="form.input" />
+        </FormItem>
+      </Form>`,
+      data() {
+        return {
+          form: {
+            input: ''
+          }
+        }
+      }
+    }, true)
+    expect(!!vm.$el.querySelector('.input-small')).toBe(true)
+  })
 })
