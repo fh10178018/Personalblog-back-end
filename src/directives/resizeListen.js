245967a1345be3b607f5c43d629f67572e886ba1
fh10178018 <1206758827@qq.com>
2020-10-11 19:58:57 +0800

    新增自定义指令，可以监听指定实例大小

---
+++ src/directives/resizeListen.js
@@ -0,0 +1,32 @@
+/**
+ * 该函数用来实时监听元素大小，可以用于自适应，或者v-show造成的宽度为0问题
+ * */
+
+import elementResizeDetectorMaker from "element-resize-detector";
+
+let observer
+const oldValue = {
+  width: 0,
+  height: 0
+}
+const resizeListen = {
+  mounted (el, binding) {
+    observer = elementResizeDetectorMaker()
+    observer.listenTo(el, function(element) {
+      const width = element.offsetWidth;
+      const height = element.offsetHeight;
+      if (oldValue.width !== width || oldValue.height !== height) { // 数值重复不回调值
+        oldValue.width = width
+        oldValue.height = height
+        binding.value(oldValue)
+      }
+    })
+  },
+  unmounted (el) {
+    if (observer) {
+      observer.removeListener(el)
+    }
+  }
+}
+
+export default resizeListen
