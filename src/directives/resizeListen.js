b884f14b346103e694a74eea2306b56ca2ccd703
fh10178018 <1206758827@qq.com>
2020-10-19 20:24:55 +0800

    修正了ESlint发现的格式错误

--- src/directives/resizeListen.js
+++ src/directives/resizeListen.js
@@ -1,8 +1,7 @@
 /**
  * 该函数用来实时监听元素大小，可以用于自适应，或者v-show造成的宽度为0问题
  * */
-
-import elementResizeDetectorMaker from "element-resize-detector";
+import elementResizeDetectorMaker from 'element-resize-detector'
 
 let observer
 const oldValue = {
@@ -12,9 +11,9 @@ const oldValue = {
 const resizeListen = {
   mounted (el, binding) {
     observer = elementResizeDetectorMaker()
-    observer.listenTo(el, function(element) {
-      const width = element.offsetWidth;
-      const height = element.offsetHeight;
+    observer.listenTo(el, function (element) {
+      const width = element.offsetWidth
+      const height = element.offsetHeight
       if (oldValue.width !== width || oldValue.height !== height) { // 数值重复不回调值
         oldValue.width = width
         oldValue.height = height
