0227a5ed0ab8b4198e4685b7a997388d7ab0e3b6
fh10178018 <1206758827@qq.com>
2020-10-21 11:58:35 +0800

    将ThreedotLoding动画重构

--- src/components/common/Loading/ThreedotLoding.vue
+++ src/components/common/Loading/ThreedotLoding.vue
@@ -1,47 +1,73 @@
 <template>
   <div class="three-dot">
-    <div class="dot dot1" :style="{backgroundColor}"></div>
-    <div class="dot dot2" :style="{backgroundColor}"></div>
-    <div class="dot dot3" :style="{backgroundColor}"></div>
+    <div v-for="(item, index) in dotList"
+         :class="item"
+         :key="index"
+         :style="dotStyle(index)"></div>
   </div>
 </template>
 
 <script>
+import {
+  ref,
+  unref,
+  reactive,
+  computed
+} from 'vue'
 export default {
   name: 'ThreedotLoading',
   props: {
     backgroundColor: {
       type: String,
-      default: 'var(--main-color)'
+      default: 'var(--theme-color)'
+    }
+  },
+  setup (props) {
+    const { backgroundColor } = unref(props)
+    const dotLeft = ref(6.4)
+    const dotList = reactive(['dot dot1', 'dot dot2', 'dot dot3'])
+
+    const dotStyle = computed(() => {
+      return index => {
+        return [
+          { left: unref(dotLeft) * index + (index + 1) * 6 + 'px' },
+          { backgroundColor: unref(backgroundColor) }
+        ]
+      }
+    })
+    return {
+      dotStyle,
+      dotList
     }
   }
 }
 </script>
 
 <style lang="less">
+.dot-wrap{
+  width: 51px;
+  height: 100%;
+}
 .three-dot{
   position: relative;
-  width: 50px;
-  height: 27px;
+  width: 51px;
+  height: 100%;
   display: inline-block;
   vertical-align: middle;
   .dot {
-    width: 10px;
-    height: 10px;
+    width: 6px;
+    height: 6px;
     border-radius: 50%;
     position: absolute;
     top: calc(50% - 5px);
   }
   .dot1 {
-    left: 0;
     animation: dot-jump 0.5s  alternate infinite;
   }
   .dot2 {
-    left: 20px;
     animation: dot-jump 0.5s 0.2s  alternate infinite;
   }
   .dot3 {
-    left: 40px;
     animation: dot-jump 0.5s 0.4s  alternate infinite;
   }
 }
