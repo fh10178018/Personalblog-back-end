93b081d285b5f3af5a0422829063b1b6e0cc655b
Mertreci <fanghan@jianyi.tech>
2020-12-03 21:48:04 +0800

    modification: add some async action

--- src/store/modules/User.js
+++ src/store/modules/User.js
@@ -1,18 +1,27 @@
 import { post, get } from 'http/index'
 import showMessage from 'components/common/Message'
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 
 // 创建响应式状态
 const userInfo = reactive({
   isAdmin: false,
-  _id: -1,
+  _id: '',
   username: '',
   name: ''
 })
-
+const AdminUserList = reactive([])
+const isLoading = reactive({
+  delete: false,
+  table: false,
+  edit: false
+})
+const totalList = ref(0)
 const state = {
   Authorization: localStorage.getItem('Authorization') ? localStorage.getItem('Authorization') : '',
-  userInfo: userInfo
+  userInfo,
+  AdminUserList,
+  isLoading,
+  totalList
 }
 
 const actions = {
@@ -23,15 +32,53 @@ const actions = {
   },
   getUserInfo (context) {
     return get('/user').then(res => {
-      console.log(context.state.userInfo)
       for (const key in res) {
         context.state.userInfo[key] = res[key]
       }
-      console.log(context.state.userInfo)
       showMessage({
         content: '你好！' + res.name,
         type: 'success'
       })
+    })
+  },
+  getAdminUserList (context, tableQuery) {
+    context.state.isLoading.table = true
+    return post('/user/getAdminUserList', tableQuery).then(res => {
+      context.commit('GETADMINUSERLIST', res.data)
+      context.commit('GETADMINUSERLISTLENGTH', res.total)
+      setTimeout(() => {
+        context.state.isLoading.table = false
+      }, 20)
+    }).catch(() => {
+      setTimeout(() => {
+        context.state.isLoading.table = false
+      }, 20)
+    })
+  },
+  deleteAdminUser (context, _id) {
+    context.state.isLoading.delete = true
+    return post('/user/deleteAdminUser', _id).then(res => {
+      context.state.isLoading.delete = false
+      context.dispatch('getAdminUserList', { page: 1, rows: 20 })
+    }).catch(() => {
+      setTimeout(() => {
+        context.state.isLoading.delete = false
+      }, 20)
+    })
+  },
+  EditAdminUser (context, operation) {
+    const { adminDate, handleModalFade } = operation
+    context.state.isLoading.edit = true
+    return post('/user/editAdminUser', adminDate).then(res => {
+      context.dispatch('getAdminUserList', { page: 1, rows: 20 })
+      setTimeout(() => {
+        context.state.isLoading.edit = false
+        handleModalFade()
+      }, 20)
+    }).catch(() => {
+      setTimeout(() => {
+        context.state.isLoading.edit = false
+      }, 20)
     })
   }
 }
@@ -45,6 +92,12 @@ const mutations = {
   LOGOUT (state) {
     localStorage.removeItem('Authorization')
     state.Authorization = null
+  },
+  GETADMINUSERLIST (state, userlist) {
+    state.AdminUserList = userlist
+  },
+  GETADMINUSERLISTLENGTH (state, total) {
+    state.totalList = total
   }
 }
 
