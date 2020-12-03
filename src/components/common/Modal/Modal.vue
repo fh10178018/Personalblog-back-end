c9c96c9f33519d28e04789fc17cc278b5deb2e25
Mertreci <fanghan@jianyi.tech>
2020-12-03 22:09:19 +0800

    modification: add some props

--- src/components/common/Modal/Modal.vue
+++ src/components/common/Modal/Modal.vue
@@ -4,27 +4,31 @@
 可以通过父访问子的方式，进行handleModalShow
 -->
 <template>
-  <transition
-    name="modal-fade"
-    @after-enter="afterEnter"
-    @after-leave="afterLeave"
-    appear>
+  <transition name="modal-fade"
+              @after-enter="afterEnter"
+              @after-leave="afterLeave"
+              appear>
     <div class="modal-wrap"
-         @click.self="handleModalFade"
+         @click.self="handleWrapFade"
          :class="{'mask': mask}"
          v-show="visable">
-      <div class="modal"
-           :class="size"
-           :style="modalStyle"
-      >
-        <button class="close-btn"  @click="handleModalFade"><i class="fa fa-close"></i></button>
-        <div class="modal-header" v-if="slots.header">
+      <div class="modal circle"
+           :class="showModalClass"
+           :style="modalStyle">
+        <button class="close-btn"
+                v-if="showClose"
+                @click="handleModalFade"><i class="fa fa-close"></i></button>
+        <div class="modal-header"
+             v-if="slots.header">
           <slot name="header"></slot>
         </div>
-        <div class="modal-content" v-if="slots.content">
+        <div class="modal-content"
+             v-loading="isLoading"
+             v-if="slots.content">
           <slot name="content"></slot>
         </div>
-        <div class="modal-footer"  v-if="slots.footer">
+        <div class="modal-footer"
+             v-if="slots.footer">
           <slot name="footer"></slot>
         </div>
       </div>
@@ -43,10 +47,18 @@ import {
 
 const useModal = (
   top,
-  width
+  width,
+  circle,
+  size,
+  loading
 ) => {
   const visable = ref(false)
-
+  const showModalClass = computed(() => {
+    return [
+      unref(circle) ? 'circle' : '',
+      unref(size)
+    ]
+  })
   const modalStyle = computed(() => {
     const style = {}
     style.marginTop = top.value
@@ -56,16 +68,25 @@ const useModal = (
     return style
   })
 
+  const isLoading = computed(() => unref(loading))
+
   return {
     visable,
-    modalStyle
+    modalStyle,
+    showModalClass,
+    isLoading
   }
 }
 
 const useInteractive = (
   visable,
-  emit
+  emit,
+  closeOnClickMask
 ) => {
+  const handleWrapFade = () => {
+    if (!unref(closeOnClickMask)) return
+    handleModalFade()
+  }
   const handleModalFade = () => {
     emit('close')
     visable.value = false
@@ -88,7 +109,8 @@ const useInteractive = (
     afterLeave,
     handleModalFade,
     handleModalShow,
-    visable
+    visable,
+    handleWrapFade
   }
 }
 
@@ -111,28 +133,51 @@ export default {
       type: String,
       default: '15vh'
     },
+    circle: {
+      type: Boolean,
+      default: false
+    },
+    showClose: {
+      type: Boolean,
+      default: true
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    closeOnClickMask: {
+      type: Boolean,
+      default: true
+    },
     width: String
   },
   emits: ['close', 'opened', 'open', 'closed'],
   setup (props, { slots, emit }) {
-    const { showModal, top, width } = toRefs(props)
+    const { showModal, top, width, circle, size, loading, closeOnClickMask } = toRefs(props)
 
     const {
       visable,
-      modalStyle
+      modalStyle,
+      showModalClass,
+      isLoading
     } = useModal(
       top,
-      width
+      width,
+      circle,
+      size,
+      loading
     )
 
     const {
       afterEnter,
       afterLeave,
       handleModalFade,
-      handleModalShow
+      handleModalShow,
+      handleWrapFade
     } = useInteractive(
       visable,
-      emit
+      emit,
+      closeOnClickMask
     )
 
     onMounted(() => {
@@ -144,90 +189,100 @@ export default {
       slots,
       handleModalFade,
       handleModalShow,
+      handleWrapFade,
       ...toRefs(props),
       afterEnter,
       afterLeave,
-      visable
+      visable,
+      showModalClass,
+      isLoading
     }
   }
 }
 </script>
 
 <style lang="less">
-  .modal-wrap{
-    z-index: 2000;
-    position: fixed;
-    overflow: auto;
-    margin: 0;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
+.modal-wrap {
+  z-index: 2000;
+  position: fixed;
+  overflow: auto;
+  margin: 0;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  transition: 500ms;
+  background-color: rgba(229, 229, 229, 0.6);
+  .circle {
+    border-radius: 10px;
+  }
+  .modal {
+    background: var(--main-color);
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+    position: relative;
+    margin: 0 auto 50px;
+    z-index: 2001;
+    overflow: hidden;
     transition: 500ms;
-    background-color: rgba(229, 229, 229, 0.6);
-    .modal{
-      background: var(--main-color);
-      box-shadow: 0 2px 5px rgba(0,0,0,.3);
-      position: relative;
-      margin: 0 auto 50px;
-      z-index: 2001;
-      .close-btn{
-        position: absolute;
-        top: 0;
-        right: 0;
-        width: 36px;
-        height: 36px;
-        background-color: transparent;
-        border: 0;
-        font-size: 25px;
-        cursor: pointer;
-      }
-      .close-btn:hover{
-        color: #d2b356;
-      }
-      .close-btn:focus{
-        outline: 0;
-      }
-      .modal-header{
-        padding: 10px;
-        font-size: 18px;
-      }
-      .modal-content{
-        padding: 10px;
-      }
-      .modal-footer{
-        padding: 10px;
-      }
+    .close-btn {
+      position: absolute;
+      top: 0;
+      right: 0;
+      width: 36px;
+      height: 36px;
+      background-color: transparent;
+      border: 0;
+      font-size: 25px;
+      cursor: pointer;
+      transition: 500ms;
+      color: var(--main-font-color);
+    }
+    .close-btn:hover {
+      color: #d2b356;
+    }
+    .close-btn:focus {
+      outline: 0;
+    }
+    .modal-header {
+      padding: 10px 20px;
+      font-size: 18px;
+      color: var(--main-font-color);
+    }
+    .modal-content {
+      padding: 10px 20px;
+    }
+    .modal-footer {
+      padding: 10px;
+      color: var(--main-font-color);
     }
   }
-  .modal.lg{
-    width: 80%;
+}
+.modal.lg {
+  width: 80%;
+}
+.modal.xs {
+  width: 50%;
+}
+.modal.sm {
+  width: 40%;
+}
+@media screen and (max-width: 830px) {
+  .modal {
+    width: 100% !important;
+    top: auto;
+    border-radius: 20px 20px 0 0;
   }
-  .modal.xs{
-    width: 50%;
-  }
-  .modal.sm{
-    width: 40%;
-  }
-  @media screen and (max-width: 830px){
-    .modal{
-      width: 100% !important;
-      top: auto;
-      border-radius: 20px 20px 0 0;
-    }
-  }
-  .modal-fade-enter-active {
+}
+.modal-fade-enter-active {
+  .modal {
     opacity: 0;
-    .modal{
-      opacity: 0;
-      transform: translateY(100%);
-    }
+    transform: scale(0.5);
   }
-  .modal-fade-leave-active {
+}
+.modal-fade-leave-active {
+  .modal {
     opacity: 0;
-    .modal{
-      opacity: 0;
-      transform: translateY(-100%);
-    }
+    transform: scale(0.5);
   }
+}
 </style>
