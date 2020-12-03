8141214e0d8fda133c82397801b9873c856ac6b2
Mertreci <fanghan@jianyi.tech>
2020-12-03 21:30:53 +0800

    modifiction: resolve login failed

--- src/views/login_page/Login.vue
+++ src/views/login_page/Login.vue
@@ -1,48 +1,51 @@
 <template>
-    <div class="login-wraper">
-      <div class="box">
-        <div class="title">个人博客后端登陆</div>
-        <Form :model="loginData" :rules="rules" ref="ruleForm">
-          <FormItem rulesName="username">
-            <InputString
-              placeholder="账号"
-              v-model="loginData.username"
-              clearable/>
-          </FormItem>
-          <FormItem rulesName="password">
-            <InputString
-              placeholder="密码"
-              type="password"
-              v-model="loginData.password"/>
-          </FormItem>
-          <FormItem rulesName="verifyRes">
-            <Verify
-              v-model="loginData.verifyRes"
-              ref="verify"
-            />
-          </FormItem>
-          <FormItem style="width: 100%;text-align: center">
-            <Button size="large" :loading="isLoading" round type="theme" :style="{width: '100%'}"  @click="submitForm">
-              <span>登陆</span>
-            </Button>
-          </FormItem>
-        </Form>
-      </div>
+  <div class="login-wraper">
+    <div class="box">
+      <div class="title">个人博客后端登陆</div>
+      <Form :model="loginData"
+            :rules="rules"
+            ref="ruleForm">
+        <FormItem rulesName="username">
+          <InputString placeholder="账号"
+                       v-model="loginData.username"
+                       clearable />
+        </FormItem>
+        <FormItem rulesName="password">
+          <InputString placeholder="密码"
+                       type="password"
+                       v-model="loginData.password" />
+        </FormItem>
+        <FormItem rulesName="verifyRes">
+          <Verify v-model="loginData.verifyRes" />
+        </FormItem>
+        <FormItem style="width: 100%;text-align: center">
+          <Button size="large"
+                  :loading="isLoading"
+                  round
+                  type="theme"
+                  :style="{width: '100%'}"
+                  @click="submitForm">
+            <span>登陆</span>
+          </Button>
+        </FormItem>
+      </Form>
     </div>
+  </div>
 </template>
 
 <script>
 import {
   ref,
   unref,
-  reactive,
-  toRefs
+  reactive
 } from 'vue'
 import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
 import InputString from 'components/common/InputString/InputString'
 import Button from 'components/common/Button/Button'
 import Verify from 'components/common/Verify/Verify'
 import FormItem from '../../components/common/FormItem/FormItem'
+import reactiveToObject from '../../utils/reactiveToObject'
 
 const useVerify = () => {
   const ruleForm = ref(null) // 用于绑定实例
@@ -85,7 +88,8 @@ const useInteractive = (
   ruleForm,
   store,
   loginData,
-  isLoading
+  isLoading,
+  router
 ) => {
   const getRuleForm = () => { // 获得实例对象
     return unref(ruleForm)
@@ -99,14 +103,14 @@ const useInteractive = (
     getRuleForm().validate((valid) => {
       if (valid) { // 验证都成功
         isLoading.value = true
-        console.log(...toRefs(loginData))
-        store.dispatch('LoginAction', { ...toRefs(loginData) }).then(res => {
-          isLoading.value = false
-          store.dispatch('getUserInfo')
-          store.commit('LOGIN', { Authorization: 'Bearer ' + res })
-          this.$router.push({ path: '/power' })
+        store.dispatch('LoginAction', reactiveToObject(loginData)).then(res => {
+          setTimeout(() => {
+            isLoading.value = false
+            store.dispatch('getUserInfo')
+            store.commit('LOGIN', { Authorization: 'Bearer ' + res })
+            router.push({ path: '/power' })
+          }, 500)
         }).catch(() => {
-          console.log('as')
           isLoading.value = false
           resetForm()
         })
@@ -126,6 +130,7 @@ export default {
   components: { FormItem, Button, InputString, Verify },
   setup () {
     const store = useStore()
+    const router = useRouter()
 
     const {
       ruleForm,
@@ -140,7 +145,8 @@ export default {
       ruleForm,
       store,
       loginData,
-      isLoading
+      isLoading,
+      router
     )
 
     return {
@@ -155,37 +161,37 @@ export default {
 </script>
 
 <style lang="less">
-  .login-wraper{
-    position: absolute;
-    top: calc(50% - 244.4px);
-    left: calc(50% - 180px);
-    box-shadow: 0 7px 25px rgba(0, 0, 0, .08);
-    height: auto;
-    transition: 0.5ms;
-    width: 360px;
-    .box{
-      background: var(--main-color);
-      height: 100%;
-      width: 100%;
-      padding: 20px;
-      border-radius: 8px;
-      .title{
-        text-align: center;
-        font-weight: 700;
-        color: var(--theme-color);
-        margin-bottom: 50px;
-        margin-top: 30px;
-      }
-      .btn-login{
-        margin-top: 30px;
-        margin-bottom: 30px;
-      }
+.login-wraper {
+  position: absolute;
+  top: calc(50% - 244.4px);
+  left: calc(50% - 180px);
+  box-shadow: 0 7px 25px rgba(0, 0, 0, 0.08);
+  height: auto;
+  transition: 0.5ms;
+  width: 360px;
+  .box {
+    background: var(--main-color);
+    height: 100%;
+    width: 100%;
+    padding: 20px;
+    border-radius: 8px;
+    .title {
+      text-align: center;
+      font-weight: 700;
+      color: var(--theme-color);
+      margin-bottom: 50px;
+      margin-top: 30px;
+    }
+    .btn-login {
+      margin-top: 30px;
+      margin-bottom: 30px;
     }
   }
-  .unfold-leave-active{
-    animation: bounce-out .5s;
-  }
-  .unfold-enter-active {
-    animation: bounce-in .5s;
-  }
+}
+.unfold-leave-active {
+  animation: bounce-out 0.5s;
+}
+.unfold-enter-active {
+  animation: bounce-in 0.5s;
+}
 </style>
