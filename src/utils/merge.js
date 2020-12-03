77b0b10439b7bd166c21811043336631de7de700
Mertreci <fanghan@jianyi.tech>
2020-12-03 21:35:25 +0800

    modifiction: resolve merge function failed

--- src/utils/merge.js
+++ src/utils/merge.js
@@ -2,11 +2,11 @@
  * 用于合并对象， 将重复的值合并掉
  * arguments和function 不可分割，用来枚举对象参数的每一个元素
  * */
-export default function merge (target) {
+export default function (target) {
   for (let i = 1, j = arguments.length; i < j; i++) {
     const source = arguments[i] || {}
     for (const prop in source) {
-      if (Object.prototype.hasOwnProperty.call(source, prop)) {
+      if (Object.hasOwnProperty.call(source, prop)) {
         const value = source[prop]
         if (value !== undefined) {
           target[prop] = value
