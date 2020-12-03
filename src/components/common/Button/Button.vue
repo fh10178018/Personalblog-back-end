2e96a2be967b0acb9c24702fecbb3ee0bb7ec6be
Mertreci <fanghan@jianyi.tech>
2020-12-03 21:55:51 +0800

    modification: resolve some style problems

--- src/components/common/Button/Button.vue
+++ src/components/common/Button/Button.vue
@@ -1,13 +1,29 @@
 <template>
-  <button @click.prevent v-if="isButton" class="btn" :class="btnClass" :style="useStyle" :disabled="buttonDisabled">
-    <threedot-loading  v-if="loading"></threedot-loading>
+  <button @click.prevent
+          v-if="isButton"
+          class="btn"
+          :class="btnClass"
+          :style="useStyle"
+          :disabled="buttonDisabled">
+    <loading-icon v-if="loading"></loading-icon>
+    <span v-if="loading">加载中</span>
     <slot v-else></slot>
   </button>
-  <div v-else-if="!buttonDisabled" class="btn" :class="btnClass" :style="useStyle">
+  <div v-else-if="!buttonDisabled"
+       class="btn"
+       :class="btnClass"
+       :style="useStyle">
     <slot></slot>
   </div>
-  <div v-else class="btn" @click.stop :class="btnClass" :style="useStyle" :disabled="buttonDisabled">
-    <threedot-loading  v-if="loading"></threedot-loading>
+  <div v-else
+       class="btn"
+       @click.stop
+       :class="btnClass"
+       :style="useStyle"
+       :disabled="buttonDisabled">
+    <loading-icon v-if="loading"></loading-icon>
+    <span v-if="loading">加载中</span>
+    <threedot-loading v-if="loading"></threedot-loading>
     <slot v-else></slot>
   </div>
 </template>
@@ -15,16 +31,19 @@
 <script>
 import { computed, inject, toRefs, unref } from 'vue'
 import ThreedotLoading from '../../common/Loading/ThreedotLoding'
+import LoadingIcon from '../LoadingIcon/LoadingIcon.vue'
 
-const useBtnClass = (size, round, type, circle, loading) => {
+const useBtnClass = (size, round, type, circle, loading, isBorder, active) => {
   const Form = inject('Form', {})
   return computed(() => {
     return [
-      'btn-' + type.value,
       'btn-' + size.value,
+      'btn-' + type.value,
       loading.value || unref(Form.loading) ? 'is-loading' : '',
       round.value && !circle.value ? 'btn-round' : '',
-      !round.value && circle.value ? 'btn-circle' : ''
+      !round.value && circle.value ? 'btn-circle' : '',
+      isBorder.value ? 'is-border' : '',
+      active.value ? 'is-active' : ''
     ]
   })
 }
@@ -71,14 +90,22 @@ export default {
     isButton: { // 是否为button类型的
       type: Boolean,
       default: true
+    },
+    border: {
+      type: Boolean,
+      default: false
+    },
+    active: {
+      type: Boolean,
+      default: false
     }
   },
-  components: { ThreedotLoading },
+  components: { ThreedotLoading, LoadingIcon },
   setup (props) {
-    const { disabled, size, round, type, loading, circle, style } = toRefs(props)
+    const { disabled, size, round, type, loading, circle, style, border, active } = toRefs(props)
     const buttonDisabled = useButtonDisabled(disabled, loading)
     const useStyle = style.value
-    const btnClass = useBtnClass(size, round, type, circle, loading)
+    const btnClass = useBtnClass(size, round, type, circle, loading, border, active)
     return {
       btnClass,
       buttonDisabled,
@@ -89,107 +116,127 @@ export default {
 </script>
 
 <style lang="less">
-  .btn{
-    border: 0;
-    text-align: center;
-    cursor: pointer;
-    user-select:none;
-  }
-  .btn:active{
-    transform: scale(0.95);
-  }
-  .btn-default{
-    color: var(--main-font-color);
-    background-color: var(--light-color);
-    transition: 500ms;
-  }
-  .btn-default:hover{
-    color: var(--main-color);
-    background-color: var(--theme-color);
-  }
-  .btn-theme{
-    color: var(--main-color);
-    background-color: var(--theme-color);
-    transition: 500ms;
-  }
-  .btn-theme:hover{
-    color: white;
-    background-color: var(--theme-color);
-    opacity: 0.8;
-  }
-  .btn-none{
-    color: var(--light-color);
-    background-color: var(--main-background-color);
-    transition: 500ms;
-  }
-  .btn-none:hover{
-    color: var(--theme-color);
-  }
-  .btn-success[disabled]{
-    color: var(--theme-color) !important;
-    background-color: var(--lighter-color) !important;
-    border: 1px solid var(--theme-color);
-    transition: 500ms;
-  }
-  .btn-error[disabled]{
-    color: var(--error-color) !important;
-    background-color: var(--lighter-error-color) !important;
-    border: 1px solid var(--error-color);
-    transition: 500ms;
-  }
-  .btn-text{
-    margin: 0;
-    padding: 0;
-    display: inline-block;
-    background-color: transparent;
-  }
-  .btn-large{
-    padding: 10px 16px;
-    font-size: 18px;
-    border-radius: 8px;
-    line-height: 1.3333333;
-  }
-  .btn-normal{
-    padding: 7px 15px;
-    font-size: 14px;
-    border-radius: 4px;
-    line-height: 1;
-  }
-  .btn-small{
-    padding: 5px 10px;
-    font-size: 12px;
-    border-radius: 4px;
-    line-height: 1;
-  }
-  .btn[disabled] {
-    color: white;
-    pointer-events: none;
-    background: var(--light-color);
-  }
-  .btn-round{
-    border-radius: 200px;
-    padding: 12px 23px;
-  }
-  .btn-circle{
-    border-radius: 50%;
-    padding: 12px;
-  }
-  .btn:focus{
-    outline: 0;
-  }
-  .is-loading {
-    background: var(--main-color) !important;
-    border: 3px solid var(--theme-color) !important;
-    width: 51px !important;
-    cursor: wait !important;
-  }
-  .is-loading.btn-large{
-    padding: 10px 0 !important;
-  }
-  .is-loading.btn-normal{
-    padding: 7px 0 !important;
-  }
-  .is-loading.btn-normal{
-    padding: 5px 0 !important;
-  }
+.btn {
+  border: 0;
+  text-align: center;
+  cursor: pointer;
+  user-select: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.btn-default:active {
+  transform: scale(0.95);
+}
+.btn-default {
+  color: var(--main-font-color);
+  background-color: var(--main-background-color);
+  transition: 500ms;
+}
+.btn-default:hover {
+  color: var(--main-color) !important;
+  background-color: var(--theme-color);
+}
+.btn-theme {
+  color: var(--main-color);
+  background-color: var(--theme-color);
+  transition: 500ms;
+}
+.btn-default[disabled],
+.btn-theme[disabled] {
+  background-color: var(--lighter-color) !important;
+}
+.btn-theme:hover {
+  color: white !important;
+  background-color: var(--theme-color);
+  opacity: 0.8;
+}
+.btn-none {
+  color: var(--light-color);
+  background-color: var(--main-background-color);
+  transition: 500ms;
+}
+.btn-none:hover {
+  color: var(--theme-color) !important;
+}
+.btn-success[disabled] {
+  color: var(--theme-color) !important;
+  background-color: var(--lighter-color) !important;
+  border: 1px solid var(--theme-color);
+  transition: 500ms;
+}
+.btn-error[disabled] {
+  color: var(--error-color) !important;
+  background-color: var(--lighter-error-color) !important;
+  border: 1px solid var(--error-color);
+  transition: 500ms;
+}
+.btn-error {
+  color: white;
+  background-color: var(--error-color);
+  border: 1px solid var(--error-color);
+  transition: 500ms;
+}
+.btn-large {
+  padding: 10px 16px;
+  font-size: 18px;
+  border-radius: 8px;
+  line-height: 1.3333333;
+}
+.btn-normal {
+  padding: 7px 9px;
+  font-size: 14px;
+  border-radius: 4px;
+  line-height: 1;
+}
+.btn-small {
+  padding: 5px 10px;
+  font-size: 12px;
+  border-radius: 4px;
+  line-height: 1;
+}
+.btn[disabled] {
+  color: var(--light-color) !important;
+  cursor: not-allowed;
+}
+.btn-round {
+  border-radius: 200px;
+  padding: 12px 23px;
+}
+.btn-circle {
+  border-radius: 50%;
+  padding: 12px;
+}
+.btn:focus {
+  outline: 0;
+}
+.btn.is-loading {
+  background-color: var(--main-background-color);
+  color: var(--theme-color);
+  cursor: wait !important;
+}
+.btn.is-border {
+  color: var(--light-color);
+  background: var(--main-color) !important;
+  border: 2px solid var(--light-color);
+}
+.btn.is-border:hover {
+  color: var(--theme-color);
+  border-color: var(--theme-color);
+}
+.btn.is-active {
+  color: var(--theme-color);
+  border-color: var(--theme-color);
+}
+.btn-text {
+  margin: 0;
+  padding: 0;
+  display: inline-block;
+  color: var(--main-font-color);
+  background-color: transparent;
+  transition: 500ms;
+}
+.btn-text:hover {
+  color: var(--theme-color);
+}
 </style>
