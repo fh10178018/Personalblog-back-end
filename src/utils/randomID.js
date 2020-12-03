0e140c438802f5d81f3eb3714d7e30e401cdf7f7
Mertreci <fanghan@jianyi.tech>
2020-12-03 21:38:10 +0800

    fix: a function to generate random number

--- src/utils/randomID.js
+++ src/utils/randomID.js
@@ -1,4 +1,4 @@
-export default function randomID() {
-  const stamp = new Date().getTime();
-  return (((1 + Math.random()) * stamp) | 0).toString(16);
+export default function randomID () {
+  const stamp = new Date().getTime()
+  return (((1 + Math.random()) * stamp) | 0).toString(16)
 }
