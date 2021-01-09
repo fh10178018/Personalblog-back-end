73f84670687c5b945c1e005840d7a898b0dff644
fh10178018 <1206758827@qq.com>
2021-01-09 09:57:53 +0800

    :lipstick: modify the style

--- src/views/functional_page/Layout.vue
+++ src/views/functional_page/Layout.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="layout-box" >
-    <Header/>
-    <SideBar/>
-    <router-view  v-slot="{ Component }">
+  <div class="layout-box">
+    <Header />
+    <SideBar />
+    <router-view v-slot="{ Component }">
       <keep-alive>
         <component :is="Component"></component>
       </keep-alive>
@@ -13,7 +13,7 @@
 
 <script>
 import Header from 'components/content/Header'
-import SideBar from 'components/common/SideBar/SideBar'
+import SideBar from 'components/content/SideBar/SideBar'
 import Footer from 'components/common/Footer/Footer'
 export default {
   name: 'Layout',
@@ -22,34 +22,36 @@ export default {
 </script>
 
 <style lang="less">
-  .layout-box{
-    height: 85vh;
-    max-width: 1300px;
-    margin: 40px auto;
-    display: grid;
-    grid-template-columns:200px 1100px;
-    grid-template-areas: 'header sidebar'
-    'header page'
-    'header footer';
-    grid-template-rows: 50px calc(85vh - 70px) 20px;
-    border-radius: 10px;
-    overflow: hidden;
-    box-shadow: 0 0 40px rgba(0,0,0,0.1);
+.layout-box {
+  height: 85vh;
+  max-width: 1300px;
+  margin: 40px auto;
+  display: grid;
+  grid-template-columns: 200px 1100px;
+  grid-template-areas:
+    "header sidebar"
+    "header page"
+    "header footer";
+  grid-template-rows: 50px calc(85vh - 70px) 20px;
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: 0 0 40px rgba(0, 0, 0, 0.1);
+}
+@media screen and (max-width: 1300px) {
+  .layout-box {
+    grid-template-columns: 50px auto;
+    grid-template-rows: 50px calc(100vh - 70px) 20px;
+    height: 100vh;
+    border-radius: 0;
+    margin: 0 auto;
   }
-  @media screen and (max-width: 1300px){
-    .layout-box{
-      grid-template-columns:50px auto;
-      grid-template-rows: 50px calc(100vh - 70px) 20px;
-      height: 100vh;
-      border-radius: 0;
-      margin: 0 auto;
-    }
-  }
-  .component-fade-enter-active, .component-fade-leave-active {
-    transition: opacity .3s ease;
-  }
-  .component-fade-enter, .component-fade-leave-to
+}
+.component-fade-enter-active,
+.component-fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.component-fade-enter, .component-fade-leave-to
     /* .component-fade-leave-active for below version 2.1.8 */ {
-    opacity: 0;
-  }
+  opacity: 0;
+}
 </style>
