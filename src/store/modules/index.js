793e0c63ea4698fedc187e823f1933866181bc7e
fh10178018 <1206758827@qq.com>
2021-01-09 10:04:25 +0800

    :heavy_plus_sign: add async request operations

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,9 +1,17 @@
 import componentsDocs from './ComponentsDocs'
 import User from './User'
 import ValidationOvertime from './ValidationOvertime'
+import Resumes from './Resumes'
+import File from './File'
+import Blog from './Blog'
+import Message from './Message'
 
 export default {
   componentsDocs,
   User,
-  ValidationOvertime
+  ValidationOvertime,
+  Resumes,
+  File,
+  Blog,
+  Message
 }
