c9266370b3c9c2c9af38e871b78fa4c1982cd863
fh10178018 <1206758827@qq.com>
2021-01-09 09:55:42 +0800

    :heavy_plus_sign: adding a DELETE request

--- src/http/index.js
+++ src/http/index.js
@@ -1,11 +1,12 @@
+import router from '@/router'
 import axios from 'axios'
 import showMessage from 'components/common/Message'
-const that = this
+import store from '../store'
 
-export const get = (url) => {
+export const get = (url, data) => {
   return new Promise((resolve, reject) => {
-    axios.get('/api' + url).then(res => {
-      resolve(res.data)
+    axios.get('/api' + url, { params: data }).then(res => {
+      resolve(res.data.data)
     }).catch(err => {
       reject(err)
     })
@@ -14,7 +15,32 @@ export const get = (url) => {
 export const post = (url, data) => {
   return new Promise((resolve, reject) => {
     axios.post(' /api' + url, data).then(res => {
-      resolve(res.data)
+      resolve(res.data.data)
+    }).catch(err => {
+      reject(err)
+    })
+  })
+}
+export const deleteAxios = (url, data) => {
+  return new Promise((resolve, reject) => {
+    axios.delete(' /api' + url, { params: data }).then(res => {
+      resolve(res.data.data)
+    }).catch(err => {
+      reject(err)
+    })
+  })
+}
+// 添加请求头  通过form添加的图片和文件的格式必须是multipart/form-data
+const uploadConfig = {
+  headers: { 'Content-Type': 'multipart/form-data' }
+}
+export const postFile = (url, name, file, description) => {
+  const params = new FormData() // 创建一个form对象
+  params.append(name, file, file.name) // append 向form表单添加数据
+  params.append('description', description) // append 向form表单添加数据
+  return new Promise((resolve, reject) => {
+    axios.post(' /api' + url, params, uploadConfig).then(res => {
+      resolve(res.data.data)
     }).catch(err => {
       reject(err)
     })
@@ -37,7 +63,7 @@ axios.interceptors.response.use(res => {
   switch (res.status) {
     case 201:
       showMessage({
-        content: httpStatus[res.status] ? httpStatus[res.status] : res.data,
+        content: httpStatus[res.status] ? httpStatus[res.status] : res.data.msg,
         type: 'success'
       })
   }
@@ -45,16 +71,19 @@ axios.interceptors.response.use(res => {
 }, error => {
   if (error.response) {
     showMessage({
-      content: httpStatus[error.response.status] ? httpStatus[error.response.status] : error.response.data,
+      content: httpStatus[error.response.status] ? httpStatus[error.response.status] : error.response.data.msg,
       type: 'error'
     })
     switch (error.response.status) {
       case 401:
-        that.$store.commit('LOGOUT')
-        that.$router.replace({
-          path: 'login',
-          query: { redirect: that.$router.currentRoute.fullPath }
-        })
+        if (error.response.data.code === 1001) {
+          store.commit('SETREFRESHSTATUS', true)
+          store.dispatch('refreshToken', localStorage.refreshAuthorization)
+        }
+        break
+      case 500:
+        router.push({ path: '/login', query: { redirect: router.currentRoute.fullPath } })
+        break
     }
   }
   return Promise.reject(error)
