423a9c21bfa4f10d5fa61794532ef78e271e1c34
fh10178018 <1206758827@qq.com>
2021-01-09 09:44:00 +0800

    :heavy_plus_sign: add Message Routing

--- src/components/content/Header.vue
+++ src/components/content/Header.vue
@@ -2,16 +2,28 @@
   <header>
     <navbar>
       <navbar-item href="/power/home">
-        <template v-slot:item-icon><i class="fa fa-home" ></i></template>
+        <template v-slot:item-icon><i class="fa fa-home"></i></template>
         <template v-slot:item-text>主页</template>
       </navbar-item>
       <navbar-item href="/power/content">
-        <template v-slot:item-icon><i class="fa fa-address-book" ></i></template>
+        <template v-slot:item-icon><i class="fa fa-edit"></i></template>
         <template v-slot:item-text>博客内容</template>
       </navbar-item>
+      <navbar-item href="/power/message">
+        <template v-slot:item-icon><i class="fa fa-edit"></i></template>
+        <template v-slot:item-text>联系信息</template>
+      </navbar-item>
       <navbar-item href="/power/docs">
-        <template v-slot:item-icon><i class="fa fa-address-book" ></i></template>
+        <template v-slot:item-icon><i class="fa fa-file-text"></i></template>
         <template v-slot:item-text>组件文档</template>
+      </navbar-item>
+      <navbar-item href="/power/usercontrol">
+        <template v-slot:item-icon><i class="fa fa-address-book"></i></template>
+        <template v-slot:item-text>用户管理</template>
+      </navbar-item>
+      <navbar-item href="/power/resumeedit">
+        <template v-slot:item-icon><i class="fa fa-address-card"></i></template>
+        <template v-slot:item-text>简历管理</template>
       </navbar-item>
     </navbar>
   </header>
@@ -27,7 +39,7 @@ export default {
 </script>
 
 <style lang="less">
-header{
+header {
   grid-area: header;
   width: 100%;
   background: var(--main-color);
@@ -35,10 +47,10 @@ header{
   height: 100%;
   z-index: 101;
   padding: 20px 30px;
-  box-shadow: 0 0 10px rgba(0,0,0,0.05), 1px 0 rgba(0,0,0,0.05);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.05), 1px 0 rgba(0, 0, 0, 0.05);
 }
 @media screen and (max-width: 1300px) {
-  header{
+  header {
     padding: 0;
   }
 }
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,15 +1,16 @@
 const Home = () => import('@/views/functional_page/Home/Home.vue')
 const Layout = () => import('@/views/functional_page/Layout.vue')
 const Login = () => import('@/views/login_page/Login.vue')
-const WebCodeEdit = () => import('@/views/edit_page/WebCodeEdit.vue')
 const Articles = () => import('../components/content/Articles/Articles.vue')
+const Message = () => import('../components/content/Message/Message.vue')
+const Messagelist = () => import('../components/content/Message/Messagelist.vue')
+const MessageDetail = () => import('../components/content/Message/MessageDetail.vue')
 const UserControl = () => import('../components/content/UserControl/UserControl.vue')
 const AdminBox = () => import('../components/content/UserControl/AdminControl/AdminBox.vue')
 const Articleslist = () => import('../components/content/Articles/Articleslist.vue')
-const ArticleEdit = () => import('@/views/edit_page/ArticleEdit.vue')
-const DirectionEdit = () => import('@/views/edit_page/DirectionEdit.vue')
 const Documents = () => import('../components/content/Documents/Documents.vue')
 const Document = () => import('../components/content/Documents/Document.vue')
+const ResumeEdit = () => import('../components/content/ResumeEdit/ResumeEdit.vue')
 const routes = [
   {
     path: '/',
@@ -49,12 +50,41 @@ const routes = [
         children: [
           {
             path: '',
-            redirect: { name: 'Articleslist', params: { sortname: 'article' } }
+            redirect: { name: 'Articleslist', params: { sortname: 'all' } }
           },
           {
             path: ':sortname',
             name: 'Articleslist',
             component: Articleslist,
+            meta: {
+              title: '内容页面'
+            }
+          }
+        ],
+        meta: {
+          title: '内容页面'
+        }
+      },
+      {
+        path: 'message',
+        component: Message,
+        children: [
+          {
+            path: '',
+            redirect: '/power/message/all'
+          },
+          {
+            path: 'all',
+            name: 'Messagelist',
+            component: Messagelist,
+            meta: {
+              title: '内容页面'
+            }
+          },
+          {
+            path: 'detail/:msgId',
+            name: 'MessageDetail',
+            component: MessageDetail,
             meta: {
               title: '内容页面'
             }
@@ -102,42 +132,12 @@ const routes = [
         meta: {
           title: '内容页面'
         }
-      }
-    ]
-  },
-  {
-    path: '/edit',
-    name: 'Edit',
-    meta: {
-      title: '个人博客后端编辑-',
-      index: 2
-    },
-    children: [
-      {
-        path: '/edit/webcodeedit',
-        name: 'WebCodeEdit',
-        component: WebCodeEdit,
-        meta: {
-          title: '代码编辑页面',
-          keepAlive: false
-        }
       },
       {
-        path: '/edit/articleedit',
-        name: 'ArticleEdit',
-        component: ArticleEdit,
+        path: 'resumeedit',
+        component: ResumeEdit,
         meta: {
-          title: '文章编辑页面',
-          keepAlive: false
-        }
-      },
-      {
-        path: '/edit/directionedit',
-        name: 'DirectionEdit',
-        component: DirectionEdit,
-        meta: {
-          title: '文章描述页面编辑',
-          keepAlive: false
+          title: '内容页面'
         }
       }
     ]
